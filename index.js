cf31abebd3dab6da2b904d285b2e0388eb070b31
isa <isaahmedshan190138@gmail.com>
2024-01-31 19:17:32 +0600

    gg

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(
 );
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173",'https://group-study-assignment-a7832.web.app'],
     credentials: true,
   })
 );
