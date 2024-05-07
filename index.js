d152b0608ba39ba204bf9823245ef159dbe1d4f2
isa <isaahmedshan190138@gmail.com>
2024-05-07 19:45:59 +0600

    gg

--- index.js
+++ index.js
@@ -33,6 +33,12 @@ app.use(
     // exposedHeaders: ["Set-Cookie"]
   })
 );
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow specified methods
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); // Allow specified headers
+  next();
+});
 app.use(express.json());
 app.use(cookieParser());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jeu0kz0.mongodb.net/?retryWrites=true&w=majority`;
