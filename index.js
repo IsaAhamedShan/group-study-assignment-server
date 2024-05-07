002b4a8a37814d0c427a3bf4d695a72bd96079c0
isa <isaahmedshan190138@gmail.com>
2024-05-07 21:09:16 +0600

    hh

--- index.js
+++ index.js
@@ -163,7 +163,7 @@ async function run() {
         .cookie("token", token, {
           httpOnly: false, //care it was true while running in local
           secure: false,
-          sameSite:'none';
+          sameSite:'none'
         })
         .send({ success: true });
     });
