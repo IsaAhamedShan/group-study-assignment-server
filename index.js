ad2b2fc37cae322b803369007f4d34480a3c83aa
isa <isaahmedshan190138@gmail.com>
2024-05-07 21:08:55 +0600

    gg

--- index.js
+++ index.js
@@ -34,7 +34,6 @@ app.use(
   })
 );
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow specified methods
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); // Allow specified headers
   next();
@@ -164,6 +163,7 @@ async function run() {
         .cookie("token", token, {
           httpOnly: false, //care it was true while running in local
           secure: false,
+          sameSite:'none';
         })
         .send({ success: true });
     });
